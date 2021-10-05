f85253052dff7494159ef61259b31c7bca53ea9a
Arash Mehdipour <arash201221@gmail.com>
2021-10-05 13:43:58 +0330

    Add project files.

---
+++ 02.EndPoint/RecepieBackOffice.UI.Web/Features/Tags/Models/AddTagRequest.cs
@@ -0,0 +1,13 @@
+﻿using RecepieBackOffice.UI.Web.Features.Common;
+
+namespace RecepieBackOffice.UI.Web.Features.Tags.Models
+{
+    public class AddTagRequest : BaseRequest
+    {
+
+        public AddTagRequest(string requestUrl) : base(requestUrl)
+        {
+        }
+        public string Title { get; set; }
+    }
+}
---
+++ 02.EndPoint/RecepieBackOffice.UI.Web/Features/Tags/Models/TagListRequest.cs
@@ -0,0 +1,11 @@
+﻿using RecepieBackOffice.UI.Web.Features.Common;
+
+namespace RecepieBackOffice.UI.Web.Features.Tags.Models
+{
+    public class TagListRequest : BaseRequest
+    {
+        public TagListRequest(string requestUrl) : base(requestUrl)
+        {
+        }
+    }
+}
